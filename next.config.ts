1422b336ba559cc7cba579d1785b0ab9bb1f09db
astowner <the1samevl@gmail.com>
2025-04-06 10:02:53 +0300

    Update config

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  typescript: {
+    ignoreBuildErrors: true
+  },
+  eslint: {
+    ignoreDuringBuilds: true
+  },
   images: {
     dangerouslyAllowSVG: true,
     remotePatterns: [
@@ -12,6 +18,12 @@ const nextConfig: NextConfig = {
   },
   experimental: {
     ppr: 'incremental',
+    after: true
+  },
+  devIndicators: {
+    appIsrStatus: true,
+    buildActivity: true,
+    buildActivityPosition: "bottom-right",
   },
 };
 
